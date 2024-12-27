9baa66887523f81271663af24d31002d20f0b69e
pdmuds4 <ryosuke3191@gmail.com>
2024-12-27 19:11:23 +0900

    yamada uiのアセットを実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { UIProvider, ColorModeScript } from "@yamada-ui/react";
+
 
 export const metadata: Metadata = {
-  title: "MHJoinUp",
-  description: "モンスターハンター参加型配信用、参加希望者管理アプリ",
+    title: "MHJoinUp",
+    description: "モンスターハンター参加型配信用、参加希望者管理アプリ",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <ColorModeScript />
+                <UIProvider>
+                    {children}
+                </UIProvider>
+            </body>
+        </html>
+    );
 }
