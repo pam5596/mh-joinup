1be361fa30c308b3f54c03df49eedde3faa738da
pdmuds4 <ryosuke3191@gmail.com>
2024-12-28 20:39:03 +0900

    mainBackground Providerを修正

--- src/components/provider/mainBackground.tsx
+++ src/components/provider/mainBackground.tsx
@@ -1,16 +1,14 @@
-import { Flex } from "@yamada-ui/react"
+import { Center } from "@yamada-ui/react"
 
 export default function MainBackground({ children }: { children: React.ReactNode }) {
     return (
         <div className="text-white bg-gradient-to-br from-orange-300 to-yellow-600 place-content-center">
-            <Flex
+            <Center
                 p={{sm: 4, md: 8, base: 12}}
-                className="min-h-screen" 
-                justify="center" 
-                items="center"
+                className="min-h-screen"
             >
                 {children}
-            </Flex>
+            </Center>
         </div>
     )
 }
