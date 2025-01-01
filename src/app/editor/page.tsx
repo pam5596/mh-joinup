f15e6d11c14c256bc70625aa57c247eaf53360f0
pdmuds4 <ryosuke3191@gmail.com>
2025-01-01 13:28:56 +0900

    待機者枠をスクロール可能にした

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SimpleGrid, GridItem, Button, Flex, Heading } from "@yamada-ui/react";
+import { SimpleGrid, GridItem, Button, Flex, Heading, ScrollArea } from "@yamada-ui/react";
 import { IoSettingsSharp } from "react-icons/io5";
 import { LuSwords } from "react-icons/lu";
 
@@ -22,14 +22,11 @@ export default function Editor() {
                 </GridItem>
                 <GridItem colSpan={1} rowSpan={2} rounded="md" bgGradient="linear(to-br, rgba(72, 85, 99, 0.6), rgba(41, 50, 60, 0.8))" p={{sm: 2, base: 4}}>
                     <Heading size="xs" color="white" mb={2}>待機者</Heading>
-                    <SimpleGrid columns={{sm: 3, base: 4}} spacing={2} gap={{sm: 2, base: 4}} justifyItems="center">
-                        <EditorAvatar />
-                        <EditorAvatar />
-                        <EditorAvatar />
-                        <EditorAvatar />
-                        <EditorAvatar />
-                        <EditorAvatar />
-                    </SimpleGrid>
+                    <ScrollArea h="230px">
+                        <SimpleGrid columns={{md:3, base: 4}} spacing={2} gap={{sm: 1, base: 4}} justifyItems="center" p={2}>
+                            { Array(20).fill(0).map((_, i) => <EditorAvatar key={i} />)}
+                        </SimpleGrid>
+                    </ScrollArea>
                 </GridItem>
                 <GridItem colSpan={1} rowSpan={1} rounded="md" bgGradient="linear(to-br, rgba(41, 50, 60, 0.8), rgba(72, 85, 99, 0.6))" p={{sm: 2, base:4}}>
                     <Flex direction="column" gap={2}>
