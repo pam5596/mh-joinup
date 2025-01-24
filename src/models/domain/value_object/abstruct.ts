11888e681abb0755d83ffeda1f2ebd801c4f1082
pdmuds4 <ryosuke3191@gmail.com>
2025-01-24 14:37:24 +0900

    AbsValueObjectをinterface->abstructに変更

--- src/models/domain/value_object/abstruct.ts
+++ src/models/domain/value_object/abstruct.ts
@@ -1,4 +1,17 @@
-export interface AbsValueObject<T> {
-    get value(): T;
-    equals(other: AbsValueObject<T>): boolean;
+export default abstract class AbsValueObject<T> {
+    private readonly _value: T;
+
+    constructor(value: T) {
+        this._value = this.validate(value);
+    }
+
+    abstract validate(const_val: T): T;
+
+    get value(): T {
+        return this._value;
+    }
+
+    equals(other: AbsValueObject<T>): boolean {
+        return this.value === other.value;
+    }
 }
---
+++ src/models/domain/value_object/users/id.ts
@@ -0,0 +1,4 @@
+import { z } from "zod";
+import { AbsValueObject } from "../abstruct";
+
+z.string().parse("a");
