aca349286323b3c4246f2cfeaac5f1dba248d265
pdmuds4 <ryosuke3191@gmail.com>
2025-01-28 10:12:19 +0900

    ManageApplicantを実装

---
+++ src/models/domain/value_object/managements/applicant.ts
@@ -0,0 +1,10 @@
+import AbsValueObject from "../abstruct";
+import { z } from "zod";
+
+export class ManageApplicant extends AbsValueObject<number> {
+    validate(const_val: number): number {
+        const schema = z.number().int().nonnegative();
+        
+        return schema.parse(const_val);
+    }
+}
