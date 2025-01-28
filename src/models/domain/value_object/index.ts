25c09524261ccc6ce7ce229df13393a544af2278
pdmuds4 <ryosuke3191@gmail.com>
2025-01-28 09:47:39 +0900

    settingsのvalueObjectをindex.tsに追記

--- src/models/domain/value_object/index.ts
+++ src/models/domain/value_object/index.ts
@@ -1,3 +1,5 @@
 export {UserGoogleId} from './users/google_id';
 export {UserAvatar} from './users/avatar';
 export {UserName} from './users/name';
+
+export {SettingKeyWord} from './settings/keyword';
