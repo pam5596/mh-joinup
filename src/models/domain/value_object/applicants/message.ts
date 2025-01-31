c2d80c5ce674da52024e0d0fbfc650b8895670be
pdmuds4 <ryosuke3191@gmail.com>
2025-01-31 12:02:02 +0900

    #22 applicantsのvalueObjectにschemaメソッドを定義

--- src/models/domain/value_object/applicants/message.ts
+++ src/models/domain/value_object/applicants/message.ts
@@ -2,9 +2,11 @@ import AbsValueObject from "../abstruct";
 import { z } from "zod";
 
 export class ApplicantMessage extends AbsValueObject<string> {
-    validate(const_val: string): string {
-        const schema = z.string().min(1).max(200);
-        
-        return schema.parse(const_val);
+    constructor(value: string) {
+        super(value, ApplicantMessage.schema());
+    }
+    
+    static schema(): z.ZodType<string> {
+        return z.string().min(1).max(200);
     }
 }
