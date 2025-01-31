b77cd39552d250155bc4c7a9f46aa44e40464689
pdmuds4 <ryosuke3191@gmail.com>
2025-01-31 11:51:31 +0900

    #22 usersのvalueObjectにschemaメソッドを定義

--- src/models/domain/value_object/users/channel_id.ts
+++ src/models/domain/value_object/users/channel_id.ts
@@ -2,9 +2,11 @@ import AbsValueObject from "../abstruct";
 import { z } from "zod";
 
 export class UserChannelId extends AbsValueObject<string> {
-    validate(const_val: string): string {
-        const schema = z.string().length(24).regex(/^[A-Za-z0-9_-]+$/)
-        
-        return schema.parse(const_val);
+    constructor(value: string) {
+        super(value, UserChannelId.schema());
+    }
+    
+    static schema(): z.ZodType<string> {
+        return z.string().length(24).regex(/^[A-Za-z0-9_-]+$/);
     }
 }
