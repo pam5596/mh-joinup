4ceb61c225831fba22d9c3b65f8e58f4e098933d
pdmuds4 <ryosuke3191@gmail.com>
2025-02-03 16:54:55 +0900

    #21 _objectsと_valuesプロパティを維持し、_objectsに型の制限を更新

--- src/models/domain/entity/abstruct.ts
+++ src/models/domain/entity/abstruct.ts
@@ -1,13 +1,21 @@
 import { z } from "zod";
 import { ObjectId } from "mongodb";
 
+import AbsValueObject from "@/models/domain/value_object/abstruct";
+
 export default abstract class AbsEntity<T, DTO> {
     protected readonly _id: ObjectId;
+    protected readonly _objects: Record<string, AbsValueObject<unknown>>;
     protected readonly _values: T;
 
-    constructor(id: ObjectId, values: T, schema: z.ZodType<T>) {
+    constructor(id: ObjectId, objects: Record<string, AbsValueObject<unknown>>, schema: z.ZodType<T>) {
         this._id = id;
-        this._values = schema.parse(values);
+        this._objects = objects
+        this._values = schema.parse(
+            Object.fromEntries(
+                Object.entries(objects).map(([_, valueObj]) => [_, valueObj.value])
+            )
+        );
     }
 
     get id(): ObjectId {
