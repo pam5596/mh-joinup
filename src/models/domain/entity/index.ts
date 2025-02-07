e8f810a525286902724c47a44a9df2a03a6b0a02
pdmuds4 <ryosuke3191@gmail.com>
2025-02-07 18:33:25 +0900

    #21 SettingEntityのtypeメンバーを追記

--- src/models/domain/entity/index.ts
+++ src/models/domain/entity/index.ts
@@ -1,2 +1,5 @@
 export {UserEntity} from './users';
 export type {UserType, UserDTO} from './users/type'
+
+export {SettingEntity} from './settings';
+export type {SettingType, SettingDTO} from './settings/type'
