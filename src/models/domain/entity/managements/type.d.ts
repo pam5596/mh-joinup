596ebae4a2e7a05ded0145bdb776787cb30f91f9
pdmuds4 <ryosuke3191@gmail.com>
2025-02-09 12:52:00 +0900

    #21 ManageEntityのTypesを定義

---
+++ src/models/domain/entity/managements/type.d.ts
@@ -0,0 +1,30 @@
+import { ObjectId } from "mongodb";
+import { ManageQuest, ManageApplicant } from "@/models/domain/value_object";
+
+export type ManageType = {
+    id: string;
+    connection_id: string;
+    joiner: ManageInstantType[];
+    waiter: ManageInstantType[];
+    quests: number;
+    applicants: number;
+}
+
+export type ManageInstantType = {
+    user_id: string;
+    quest: number;
+}
+
+export type ManageDTO = {
+    id: ObjectId;
+    connection_id: ObjectId;
+    joiner: ManageInstantDTO[];
+    waiter: ManageInstantDTO[];
+    quests: ManageQuest;
+    applicants: ManageApplicant;
+}
+
+export type ManageInstantDTO = {
+    user_id: ObjectId;
+    quest: ManageQuest;
+}
