9de50707566a90761a89af3c05a4eb56102ee388
pdmuds4 <ryosuke3191@gmail.com>
2025-06-12 20:04:20 +0900

    #23 ManagementRepositoryを実装

---
+++ src/models/infrastructure/repository/management.ts
@@ -0,0 +1,49 @@
+import AbsRepository from "./abstruct";
+import { ManageEntity } from "@/models/domain/entity";
+import { ManageInstant } from "@/models/domain/embedded";
+import { ManageApplicant, ManageQuest } from "@/models/domain/value_object";
+
+import { Collection, ObjectId } from "mongodb";
+import MongoDBClient from "../client/mongodb";
+
+
+export default class ManagementRepository extends AbsRepository<ManageEntity> {
+    constructor(client: MongoDBClient, collection: Collection) {
+        super(client, collection);
+    }
+
+
+    async insert(entity: ManageEntity): Promise<void> {
+        await this.insertRaw(entity);
+    }
+
+
+    async selectNewOneByConnectionId(connection_id: ObjectId){//: Promise<ManageEntity[]|null> {
+        const result = await this.selectNewOneByOtherPropsRaw("connection_id", connection_id.toHexString())
+        
+        if (result) {
+            return new ManageEntity(
+                result._id,
+                new ObjectId(result.connection_id),
+                result.joiner.map(
+                    (joiner: { user_id: string, quest: number }) => 
+                        new ManageInstant(
+                            new ObjectId(joiner.user_id),
+                            new ManageQuest(joiner.quest)
+                        )
+                ),
+                result.waiter.map(
+                    (waiter: { user_id: string, quest: number }) => 
+                        new ManageInstant(
+                            new ObjectId(waiter.user_id),
+                            new ManageQuest(waiter.quest)
+                        )
+                ),
+                new ManageQuest(result.quests),
+                new ManageApplicant(result.applicants)
+            );
+        } else {
+            return null
+        }
+    }
+}
