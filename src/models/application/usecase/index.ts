08efb5cdbefe93e3ee2348cfba8082b4523d5b70
pdmuds4 <ryosuke3191@gmail.com>
2025-06-14 17:26:43 +0900

    #31 GetSettingUseCaseを実装

--- src/models/application/usecase/index.ts
+++ src/models/application/usecase/index.ts
@@ -1,4 +1,4 @@
 export { default as AddApplicantUseCase } from "./add_applicant";
 export { default as AddManagementUseCase } from "./add_management";
-
+export { default as GetSettingUseCase } from "./get_setting";
 export { default as GetUserUseCase } from "./get_user";
