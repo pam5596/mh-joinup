2383c90f440af69051b1115daeb231c9125e8035
pdmuds4 <ryosuke3191@gmail.com>
2025-06-21 13:44:14 +0900

    #35 GetChannelInfoServiceを実装

---
+++ src/models/application/service/get_channel_info.ts
@@ -0,0 +1,26 @@
+import AbsService from "./abstruct";
+import { ServiceError } from "@/models/error";
+
+import { TokenResponse } from "@react-oauth/google";
+import { youtube_v3 } from "googleapis";
+
+
+export default class GetChannelInfo extends AbsService<TokenResponse, youtube_v3.Schema$ChannelProfileDetails> {
+    async execute(request: TokenResponse): Promise<youtube_v3.Schema$ChannelProfileDetails> {
+        const response = await fetch("https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${request.access_token}`,
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) throw new ServiceError(
+            "ユーザチャンネルの取得に失敗しました",
+            request,
+            response.status,
+        )
+
+        return await response.json();
+    }
+}
--- src/models/application/service/index.ts
+++ src/models/application/service/index.ts
@@ -1,1 +1,2 @@
 export { default as GetUserIdService } from "./get_user_id";
+export { default as GetChannelInfoService } from "./get_channel_info";
