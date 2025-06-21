4e58442a8ad0e2171501496bc0d7b14d772aae08
pdmuds4 <ryosuke3191@gmail.com>
2025-06-21 13:39:58 +0900

    #35 ServiceErrorを実装

--- src/models/error/index.ts
+++ src/models/error/index.ts
@@ -2,3 +2,4 @@ export { default as ValueObjectError } from './value_object';
 export { default as RepositoryError } from './repository';
 export { default as ClientError } from './client';
 export { default as UseCaseError } from './usecase';
+export { default as ServiceError } from './service';
