597b95b0081558c686d14d49f6e62335d4c5c048
pdmuds4 <ryosuke3191@gmail.com>
2025-06-21 18:11:50 +0900

    #35 GetUserUsecaseのRequestpayloadを変更

--- src/models/application/payload/get_user/request.ts
+++ src/models/application/payload/get_user/request.ts
@@ -1,27 +1,28 @@
 import { z } from "zod";
+import { ObjectId } from "mongodb";
 
 import AbsPayload from "../abstruct";
-import { UserChannelId } from "@/models/domain/value_object";
+import { MongoId } from "@/models/domain/value_object";
 import type { GetUserRequestDTO, GetUserRequestType } from "./type";
 
 
 export class GetUserRequestPayload extends AbsPayload<GetUserRequestType, GetUserRequestDTO> {
-    protected channel_id: UserChannelId;
-
-    constructor(channel_id: UserChannelId) {
-        super({ channel_id: channel_id.value }, GetUserRequestPayload.schema());
-        this.channel_id = channel_id;
+    protected user_id: ObjectId;
+    
+    constructor(user_id: ObjectId) {
+        super({ user_id: user_id.toHexString() }, GetUserRequestPayload.schema());
+        this.user_id = user_id;
     }
 
     static schema() {
         return z.object({
-            channel_id: UserChannelId.schema()
+            user_id: MongoId.schema()
         });
     }
 
     toObject(): GetUserRequestDTO {
         return {
-            channel_id: this.channel_id
+            user_id: this.user_id
         };
     }
 }
--- src/models/application/payload/get_user/type.d.ts
+++ src/models/application/payload/get_user/type.d.ts
@@ -1,8 +1,12 @@
 import type { UserType, UserDTO } from "@/models/domain/entity";
 
-export type GetUserRequestType = Pick<UserType, 'channel_id'>;
+export type GetUserRequestType = {
+    user_id: UserType['id'];
+}
 
-export type GetUserRequestDTO = Pick<UserDTO, 'channel_id'>;
+export type GetUserRequestDTO = {
+    user_id: UserDTO['id'];
+}
 
 export type GetUserResponseType = Omit<UserType, 'id'>
 
