c7d9fd9dac1744b8584d0aedc104096d64201d27
pdmuds4 <ryosuke3191@gmail.com>
2025-06-21 18:14:24 +0900

    #35 GoogleOauthProviderにログインチェックイベントを実装

--- src/components/provider/googleOauth.tsx
+++ src/components/provider/googleOauth.tsx
@@ -1,4 +1,32 @@
+"use client";
+import { useEffect } from "react";
+import { useCallApi } from "@/hooks"
+import { redirect } from "next/navigation";
+
 export default function GoogleOauth({ children }: { children: React.ReactNode }) {
+    const { fetchAPI } = useCallApi();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetchAPI(
+                '/api/google-oauth',
+                'GET',
+                undefined,
+                {
+                    title: '正常にログインしました',
+                    description: 'GoogleOAuthの認証が成功しました。',
+                },
+                undefined,
+                undefined,
+                () => {
+                    redirect('/')
+                }
+            );
+        };
+        fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <>
             {children}
