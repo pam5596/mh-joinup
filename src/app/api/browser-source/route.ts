9598d64ac2373e55ac1e0aa5f229416efd17e6fe
pdmuds4 <ryosuke3191@gmail.com>
2025-06-28 14:07:10 +0900

    #35 /api/browser-sourceにchannel_idクエリパラメータを付与するよう変更

--- src/app/api/browser-source/route.ts
+++ src/app/api/browser-source/route.ts
@@ -1,16 +1,22 @@
 import errorHandling from "@/utils/errorHandling";
 
+import MongoDBClient from "@/models/infrastructure/client/mongodb";
 import { BrowserSourceGETUseCase } from "@/models/application/usecase";
 import { CookieParseService, CreateHashedIdService } from "@/models/application/service";
+import { UserRepository } from "@/models/infrastructure/repository";
+
+const mongoDBClient = new MongoDBClient(process.env.MONGODB_URI || "");
+const collection = mongoDBClient.field(process.env.MONGODB_NAME || '', "users");
+const userRepository = new UserRepository(mongoDBClient, collection);
 
 const cookieParseService = new CookieParseService();
 const createHashedIdService = new CreateHashedIdService();
-
 
 export async function GET(request: Request) {
     return errorHandling(request, async (request) => {
         const usecase = new BrowserSourceGETUseCase(
             request,
+            userRepository,
             cookieParseService,
             createHashedIdService
         );
--- src/models/application/usecase/browser-source/get.ts
+++ src/models/application/usecase/browser-source/get.ts
@@ -1,19 +1,25 @@
 import AbsUseCase from "../abstruct";
+import { UseCaseError } from "@/models/error";
 
 import { BrowserSourcePayload } from "../../payload";
 import { CookieParseService, CreateHashedIdService } from "@/models/application/service";
+import { UserRepository } from "@/models/infrastructure/repository";
 
 
 export default class BrowserSourceGETUseCase extends AbsUseCase<BrowserSourcePayload.GETRequestType, BrowserSourcePayload.GETResponseType> {
+    userRepository: UserRepository
     cookieParseService: CookieParseService;
     createHashedIdService: CreateHashedIdService;
 
+
     constructor(
         request: Request,
+        userRepository: UserRepository,
         cookieParseService: CookieParseService,
         createHashedIdService: CreateHashedIdService
     ) {
         super(request)
+        this.userRepository = userRepository
         this.cookieParseService = cookieParseService
         this.createHashedIdService = createHashedIdService
     }
@@ -21,10 +27,19 @@ export default class BrowserSourceGETUseCase extends AbsUseCase<BrowserSourcePay
     async execute(): Promise<BrowserSourcePayload.GETResponseType> {
         const url_origin = new URL(this.request.url).origin;
         const { user_id } = await this.cookieParseService.execute(this.request)
+
         const hashed_user_id = await this.createHashedIdService.execute(user_id)
 
+        const selected_user = await this.userRepository.selectById(user_id);
+
+        if (!selected_user) throw new UseCaseError(
+            'ユーザが見つかりませんでした。ログインし直してください。',
+            this.request.url,
+            404
+        )
+
         return {
-            url: `${url_origin}/browser-source?hash=${hashed_user_id}`
+            url: `${url_origin}/browser-source?hash=${hashed_user_id}&channel=${selected_user.channel_id.value}`
         }
     }
 }
