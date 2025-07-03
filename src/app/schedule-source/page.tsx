c914e5b1fa5dc5b8c9dda6195f3880cc9551996c
pdmuds4 <ryosuke3191@gmail.com>
2025-07-03 18:17:47 +0900

    #49 /schedule-sourceをsuspenseで包括

--- src/app/schedule-source/page.tsx
+++ src/app/schedule-source/page.tsx
@@ -1,15 +1,11 @@
 "use client";
-import { MainBackgroundProvider } from "@/components/provider";
+import { Suspense } from "react";
 import ScheduleCard from "./scheduleCard";
 
-import { useScheduleSourceController } from "../controller";
-
 export default function ScheduleSource() {
-    const { user_info, events, this_week_days } = useScheduleSourceController(); 
-
     return (
-        <MainBackgroundProvider>
-            <ScheduleCard user_info={user_info} events={events} this_week_days={this_week_days}/>
-        </MainBackgroundProvider>
+        <Suspense>
+            <ScheduleCard />
+        </Suspense>
     )
 }
